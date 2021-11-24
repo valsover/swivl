6d4eec091003cf57ff0084502c7d8bda2dffd5e1
valsover <68599608+valsover@users.noreply.github.com>
2021-11-24 16:41:15 +0200

    feat: add interactivity to a drop-down menu

---
+++ script.js
@@ -0,0 +1,17 @@
+//Drop-down menu
+
+const dotsBtn = document.getElementById('dotsBtn'),
+  dropDownMenu = document.querySelector('.left__menu'),
+  menuBtns = document.querySelectorAll('.left__menu p');
+
+const showDropDown = (e) => {
+  console.log(dropDownMenu.classList.length);
+  if (dropDownMenu.classList.length === 3) {
+    dropDownMenu.classList.remove('left__menu_hidden');
+  } else {
+    dropDownMenu.classList.add('left__menu_hidden');
+  }
+}
+
+dotsBtn.addEventListener('click', showDropDown);
+menuBtns.forEach((el) => el.addEventListener('click', showDropDown));
