a1d28dae1c19b0add062d1dd5b838f05ee79d331
valsover <68599608+valsover@users.noreply.github.com>
2021-11-25 17:16:28 +0200

    feat: add interactive burger menu

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ const dotsBtn = document.getElementById('dotsBtn'),
   menuBtns = document.querySelectorAll('.left__menu p');
 
 const showDropDown = (e) => {
-  if (dropDownMenu.classList.length === 3) {
-    dropDownMenu.classList.remove('left__menu_hidden');
-  } else {
-    dropDownMenu.classList.add('left__menu_hidden');
-  }
+  (dropDownMenu.classList.length === 3) ? dropDownMenu.classList.remove('left__menu_hidden') : dropDownMenu.classList.add('left__menu_hidden');
 }
 
 dotsBtn.addEventListener('click', showDropDown);
 menuBtns.forEach((el) => el.addEventListener('click', showDropDown));
+
+
+
+//Burger menu
+
+const burgerBtn = document.getElementById('burgerBtn'),
+  headerNav = document.querySelector('.header__left-side'),
+  navList = headerNav.querySelector('.nav__list'),
+  listItem = headerNav.querySelectorAll('.nav__list-item');
+
+const showNavList = (e) => {
+  (navList.classList.length === 1) ? navList.classList.add('nav__list_open') : navList.classList.remove('nav__list_open');
+}
+
+burgerBtn.addEventListener('click', showNavList);
+listItem.forEach((el) => el.addEventListener('click', showNavList));
